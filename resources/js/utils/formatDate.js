20efffb5e450a34a998cc2d7a7c444748cd484a1
Akbar Hossam Delmiro <akbarhossam123@gmail.com>
2025-06-02 00:35:25 +0700

    utils formatCurrentcy and dateTime

---
+++ resources/js/utils/formatDate.js
@@ -0,0 +1,9 @@
+export const formatDate = (dateString) => {
+    if (!dateString) return '-';
+    const options = {
+        year: 'numeric',
+        month: 'long',
+        day: 'numeric'
+    };
+    return new Date(dateString).toLocaleDateString('id-ID', options);
+};
